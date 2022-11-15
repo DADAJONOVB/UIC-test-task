d987fc95034d7a09cf3f83b8f9e9d78c9465d89e
DADAJONOVB <dadajonovboburjon3@gmail.com>
2022-11-15 10:39:07 +0300

    documentation

--- Metsenat/urls.py
+++ Metsenat/urls.py
@@ -22,11 +22,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Metsenat API",
       default_version='v1',
-      description="Test description",
+      description="API for test task",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
+      contact=openapi.Contact(email="dadajonovboburjon3@gmail.com"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
